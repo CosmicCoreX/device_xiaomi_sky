c7dc52f5db9f122553bf8ae564d3d2a78dcdd03d
Lostark13 <tushar9915@gmail.com>
2026-01-26 11:11:03 +0000

    sky: rro_overlay: CarrierConfig -> CarrierConfigRes
    This will fix apkcerts packaging failure becouse build failing during PACKAGING/apkcerts due to a mismatch between Soong and Make generated apkcerts lists.
    
    Error:
    Files lineage_sky-apkcerts-soong-doublecheck.txt and
    lineage_sky-apkcerts-soong_apkcerts_file_with_soong_and_make_modules_removed.txt differ

--- device.mk
+++ device.mk
@@ -351,7 +351,7 @@ PRODUCT_PACKAGES += \
 
 # RRO Overlays
 PRODUCT_PACKAGES += \
-    CarrierConfig \
+    CarrierConfigRes \
     FrameworksResCommon \
     FrameworksResTarget \
     LauncherOverlayTarget \
